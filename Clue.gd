e21f903005b41ee6c76f6644cb331def339c8a18
JohnWalt1 <jovanamadeo07@gmail.com>
2026-07-16 22:16:14 +0700

    Selesai membuat intro base, & clue to story

--- Clue.gd
+++ Clue.gd
@@ -4,6 +4,9 @@ extends Area2D
 @export var minigame_id:String=""
 @export var minigame_data:Dictionary={}
 var player_ref:Node2D=null
+@export var trigger_story: DialogicTimeline=null
+@export var dialog_entries: Array[DialogCondition]=[]
+@export var is_puzzle:bool=false
 func _ready():
 	pass
 	
@@ -11,10 +14,38 @@ func _ready():
 func _input(event:InputEvent):
 	if event.is_action_pressed("interact") and has_overlapping_bodies():
 		var bodies=get_overlapping_bodies()
+		InventoryManager.add_item(item_data,1)
+		Global.pause_gameplay()
+		var entry:=_resolve_entry()
+		if entry==null or entry.lines.is_empty():
+			Global.resume_gameplay()
+			print("Tidak ada lah :v")
+			return
+		DialogManager.dialog_ended.connect(_on_custom_dialog_finished, CONNECT_ONE_SHOT)
+		DialogManager.start_dialog(global_position, entry.lines)
 		for body in bodies:
-			if body.is_in_group("player"):
+			if body.is_in_group("player") and is_puzzle:
 				trigger_minigame()
 				break
+func _on_custom_dialog_finished():
+	Global.resume_gameplay()
+	if trigger_story!=null:
+		trigger_dialogic()
+func _resolve_entry() -> DialogCondition:
+	for entry in dialog_entries:
+		var result:=dialog_entries
+		if result:
+			return entry
+	return null
+
+func trigger_dialogic():
+	Global.pause_gameplay()
+	Dialogic.start("Arrest")
+	Dialogic.timeline_ended.connect(_on_arrest_ended, CONNECT_ONE_SHOT)
+
+func _on_arrest_ended():
+	Global.resume_gameplay()
+	Dialogic.VAR.act1_completed=true
 
 func trigger_minigame():
 	MinigameManager.start_minigame(minigame_id,minigame_data)
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,9 +2,20 @@ extends Node
 
 @onready var button: Button = $UI/Button
 @onready var usable_panel: UsablePanel = $UI/UsablePanel
-
+var is_begin=true
 func _ready():
 	button.pressed.connect(_open_usable_popup)
-
+	if not Dialogic.VAR.act1_completed:
+		start_tutorial_intro()
+	else:
+		pass
 func _open_usable_popup():
 	usable_panel.popup_centered()
+	
+func start_tutorial_intro():
+	get_node("ysort").process_mode=Node.PROCESS_MODE_DISABLED
+	
+	Dialogic.start("Intro")
+	Dialogic.timeline_ended.connect(_on_intro_ended, CONNECT_ONE_SHOT)
+func _on_intro_ended():
+	get_node("ysort").process_mode=Node.PROCESS_MODE_INHERIT
